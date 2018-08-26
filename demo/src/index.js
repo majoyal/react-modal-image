62facf6c6e8720d6506142b0cb132f1f859ecfcf
Marc-André Joyal <majoyal@karbonik.ca>
2018-08-26 17:01:54 -0400

    Reformat demo code and update info link

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,54 +1,54 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import ModalImage from "../../src";
+import ModalImage from '../../src';
 
 class Demo extends Component {
-  render() {
-    return (
-      <div>
-        <h1>react-modal-image</h1>
+    render() {
+        return (
+            <div>
+                <h1>react-modal-image</h1>
 
-        <h2>#1 with alt, small, medium and large props</h2>
+                <h2>#1 with alt, small, medium and large props</h2>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            alt="Here is the caption"
-            small="example_img_small.jpg"
-            medium="example_img_medium.jpg"
-            large="example_img_large.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+                <div style={{ maxWidth: '400px' }}>
+                    <ModalImage
+                        alt="Here is the caption"
+                        small="example_img_small.jpg"
+                        medium="example_img_medium.jpg"
+                        large="example_img_large.jpg"
+                    />
+                </div>
+                <p>^ click or inspect the image above</p>
 
-        <h2>#2 with small and large props defined only</h2>
+                <h2>#2 with small and large props defined only</h2>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            large="example_img_large.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+                <div style={{ maxWidth: '400px' }}>
+                    <ModalImage
+                        small="example_img_small.jpg"
+                        large="example_img_large.jpg"
+                    />
+                </div>
+                <p>^ click or inspect the image above</p>
 
-        <h2>#3 with small and medium props defined only</h2>
+                <h2>#3 with small and medium props defined only</h2>
 
-        <div style={{ maxWidth: "400px" }}>
-          <ModalImage
-            small="example_img_small.jpg"
-            medium="example_img_medium.jpg"
-          />
-        </div>
-        <p>^ click or inspect the image above</p>
+                <div style={{ maxWidth: '400px' }}>
+                    <ModalImage
+                        small="example_img_small.jpg"
+                        medium="example_img_medium.jpg"
+                    />
+                </div>
+                <p>^ click or inspect the image above</p>
 
-        <h2>Further info</h2>
+                <h2>Further info</h2>
 
-        <p>
-          <a href="https://github.com/aautio/react-modal-image">Github</a>
-        </p>
-      </div>
-    );
-  }
+                <p>
+                    <a href="https://github.com/majoyal/react-modal-image">Github</a>
+                </p>
+            </div>
+        );
+    }
 }
 
-render(<Demo />, document.querySelector("#demo"));
+render(<Demo/>, document.querySelector('#demo'));
